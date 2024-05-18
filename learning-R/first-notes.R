317aae7c4f7324cf5d5f589b2eb65d00fc926ee5
NavU0821 <navaneeth12245@gmail.com>
2024-05-17 20:29:43 -0400

    main

---
+++ learning-R/first-notes.R
@@ -0,0 +1,40 @@
+# 1. Entering Input
+
+# Here, we can type expressions. The (<=) is the assignment operator
+
+x <- 12
+
+# Example 1
+print(x)
+
+# prints out [1] 12. 
+x 
+# Also prints out [1] 12 (or whatever numerical value you have assigned to x) => also known as autoprinting
+
+#[1] indicates that x is a vector and that 12 (or any assigned numerical value in the original expression is the first value)
+
+# [:] operator is used to create sequences of integers. For example, 
+
+x <- 1:20 
+x 
+# This would then print out [1] 1, 2, 3, 4, 5...... and so on. It would list ALL INTEGERS within the specific interval.
+
+# 2: Primitive Data Types: Objects and Attributes
+
+    # a) Characters
+    # b) Numerics (Real/Decimal Numbers)
+    # c) Integers
+    # d) Complex 
+    # e) Logical (True/False values are only applicable)
+
+    # ^The most basic object of all is a vector. **The only exception is a list => which is represented as a vector. 
+    # However, a list can hold data (objects) of different classes. 
+
+# Empty Vectors
+
+vector()
+
+
+
+
+
