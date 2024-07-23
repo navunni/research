1334ea8fe4999ddb310d26419fdef5ceeaf4732a
Navaneeth Unnikrishnan <nu2071@nyu.edu>
2024-07-23 12:37:45 -0400

    removed the line of best fit from visualization

--- code/dhs-daily-visitors-analysis.py
+++ code/dhs-daily-visitors-analysis.py
@@ -1,4 +1,5 @@
 import pandas as pd
+import numpy as np
 import matplotlib.pyplot as plt
 
 filepath = "/Users/navunni/Desktop/research/data/dhs-daily-report-updates.csv"
@@ -7,11 +8,21 @@ data = pd.read_csv(filepath)
 print("Column Names", data.columns)
 
 data['Date of Census'] = pd.to_datetime(data['Date of Census'])
+data = data.sort_values('Date of Census')
+
+x = data['Date of Census'].map(pd.Timestamp.toordinal)
+y = data['Total Individuals in Shelter']
+
+coefficients = np.polyfit(x, y, 1)
+poly = np.poly1d(coefficients)
+
+x_fit = np.linspace(x.min(), x.max(), 100)
+y_fit = poly(x_fit)
 
 summary_stats = data.describe()
 print (summary_stats)
 
-plt.figure(figsize=(20, 10))
+plt.figure(figsize=(18, 10))
 plt.plot(data['Date of Census'], data['Total Individuals in Shelter'], label='Total Individuals in Shelter')
 plt.plot(data['Date of Census'], data['Total Single Adults in Shelter'], label='Total Single Adults in Shelter')
 plt.plot(data['Date of Census'], data['Total Individuals in Families with Children in Shelter '], label='Total Individuals in Families with Children in Shelter')
